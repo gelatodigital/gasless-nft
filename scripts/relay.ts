6e5e0cd0b17880a8ae20f8a183c12a67c1cd4691
gitpusha <gitpusha@gmail.com>
2023-04-11 18:46:19 +0200

    chore: cleanup

--- scripts/relay.ts
+++ scripts/relay.ts
@@ -1,17 +1,21 @@
+import hre from "hardhat";
 import { BytesLike, Contract, Wallet } from "ethers";
-import hre, { ethers } from "hardhat";
 
-import {  GelatoRelay } from "@gelatonetwork/relay-sdk";
+import { GelatoRelay } from "@gelatonetwork/relay-sdk";
 const relay = new GelatoRelay();
 
 async function main() {
-  const deployer_provider = hre.ethers.provider;
+  const deployerProvider = hre.ethers.provider;
   const privKeyDEPLOYER = process.env["PRIVATE_KEY"] as BytesLike;
-  const deployer_wallet = new Wallet(privKeyDEPLOYER);
-  const deployer = await deployer_wallet.connect(deployer_provider);
+  const deployerWallet = new Wallet(privKeyDEPLOYER);
+  const deployer = await deployerWallet.connect(deployerProvider);
 
-  const NftRelayABI = ["function mint() external"];
-  const nftRelay = new Contract("0xD86B17901326C24057cA0CE9BaCBA85dB170f242", NftRelayABI, deployer);
+  const gaslessNftAbi = ["function mint() external"];
+  const nftRelay = new Contract(
+    "0xD86B17901326C24057cA0CE9BaCBA85dB170f242",
+    gaslessNftAbi,
+    deployer
+  );
 
   const { data } = await nftRelay.populateTransaction.mint();
 
@@ -24,10 +28,13 @@ async function main() {
 
   const sponsorApiKey = process.env.SPONSOR_KEY ?? "";
 
-  const relayResponse = await relay.sponsoredCallERC2771(request, deployer, sponsorApiKey);
+  const relayResponse = await relay.sponsoredCallERC2771(
+    request,
+    deployer,
+    sponsorApiKey
+  );
 
-
-  let taskId = relayResponse.taskId;
+  const taskId = relayResponse.taskId;
 
   console.log(`https://relay.gelato.digital/tasks/status/${taskId}`);
 }
